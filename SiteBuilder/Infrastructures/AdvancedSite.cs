2fae676577c6ccf809fba945706a706987293994
Westley Rotolo <westley.rotolo@capgemini.com>
2022-08-17 01:26:08 +0200

    add infrastructures

---
+++ SiteBuilder/Infrastructures/AdvancedSite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Models;
+
+namespace SiteBuilder.Infrastructures
+{
+    [SiteType(Title = "Advanced Site")]
+    public class AdvancedSite : SiteContent<AdvancedSite>
+    {
+        [Region(Title = "Footer", Display = RegionDisplayMode.Setting)]
+        public Footer FooterContents { get; set; }
+    }
+}
+
---
+++ SiteBuilder/Infrastructures/Footer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+
+namespace SiteBuilder.Infrastructures
+{
+    public class Footer
+    {
+        [Field(Title = "Mostra Chi Siamo")]
+        public CheckBoxField ShoWhoWeAre { get; set; }
+        [Field(Title = "Chi Siamo")]
+        public HtmlField WhoWeAre { get; set; }
+        [Field(Title = "Mostra Social")]
+        public CheckBoxField ShowSocial { get; set; }
+        [Field(Title = "Facebook")]
+        public TextField Facebook { get; set; }
+        [Field(Title = "Twitter")]
+        public TextField Twitter { get; set; }
+        [Field(Title = "Instagram")]
+        public TextField Instagram { get; set; }
+    }
+}
+
